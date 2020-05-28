f58a12ed0e7497acc1a563d59aad4444f4815a5a
rafcik <pasiutrafal@gmail.com>
2020-05-28 19:10:32 +0200

    ng build github-actions --prod

--- apps/github-actions/src/app/app.component.ts
+++ apps/github-actions/src/app/app.component.ts
@@ -3,16 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'architech-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   ngOnInit(): void {
-    const a = 2;
-
-    if (a == 4) {
-      console.log('its four');
-    } else {
-      console.log('its not four');
-    }
   }
 }
